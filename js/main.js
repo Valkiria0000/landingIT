4cbd48cbf457ae542c1094861edba59683d5760d
Valkiria0000 <valkiria0000@bk.ru>
2021-11-28 23:15:12 +0300

    подключен js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+  
+   loop: true,
+ 
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ 
+  
+ });
